b8e84a5036917d451b8caac2900d6868573aa8b6
hjh0207 <hjh0207@gmail.com>
2023-06-19 20:39:23 +0900

    remove

--- naverapi.py
+++ naverapi.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import pandas as pd
+import html
 
 CLIENT_ID = '3q5CdnaIhuYAlTI0rNRK'
 CLIENT_SECRET = '20ZmNFLdPM'
@@ -22,7 +23,8 @@ def translate(line):
     response = requests.post(url2, headers=headers, data=data_str.encode('utf-8'))
     if response.status_code == 200:
         result = response.json().get('message', {}).get('result', {})
-        return result.get('translatedText')
+        translated_text = result.get('translatedText')
+        return html.unescape(translated_text)
     else:
         return None
 
@@ -39,16 +41,15 @@ def get_news_data(query):
         datas = []
         for i in items:
             data = {
-                'title': i['title'].replace("&apos;", "'"),
-                'pubDate': i['pubDate'].replace("&apos;", "'"),
-                'originallink': i['originallink'].replace("&apos;", "'"),
-                'description': i['description'].replace("&apos;", "'"),
-                'papago': translate(i['title'].replace("&apos;", "'"))
+                'title': html.unescape(i['title']),
+                'pubDate': html.unescape(i['pubDate']),
+                'originallink': html.unescape(i['originallink']),
+                'description': html.unescape(i['description']),
+                'papago': translate(html.unescape(i['title']))
             }
             print(i['title'])
             datas.append(data)
         return pd.DataFrame(datas)
-
 
 data = get_news_data('경성대')
 data.to_csv('translate.csv', index=False)
